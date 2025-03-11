4ab289c18849d1251a3fa2bcaa848b8f7fb97217
miginho12 <jinhoya89@gmail.com>
2025-03-12 01:49:29 +0900

    feature/commit_rule update

---
+++ src/main/java/org/example/gangazido_be/GangazidoBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.gangazido_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangazidoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GangazidoBeApplication.class, args);
+	}
+
+}
