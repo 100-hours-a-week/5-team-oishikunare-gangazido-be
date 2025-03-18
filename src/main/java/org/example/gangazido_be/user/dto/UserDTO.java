36c7d21f4dfafe7ac0e1d80a30cd8bb3397fecb7
SeHyeonLee-dev <sktpgus2023@gmail.com>
2025-03-18 13:14:31 +0900

    feat: [POST] /v1/users/signup, [POST] /v1/users/login, [POST] v1/users/logout, [GET] v1/users/me API 구현

---
+++ src/main/java/org/example/gangazido_be/user/dto/UserDTO.java
@@ -0,0 +1,32 @@
+package org.example.gangazido_be.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDTO {
+	@NotEmpty(message = "이메일은 필수 입력 항목입니다.")
+	@Email(message = "유효한 이메일 형식이 아닙니다.")
+	@JsonProperty("user_email")
+	private String email;
+
+	@NotEmpty(message = "비밀번호는 필수 입력 항목입니다.")
+	@Size(min = 8, max = 20, message = "비밀번호는 8자 이상, 20자 이하여야 합니다.")
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
+		message = "비밀번호는 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.")
+	@JsonProperty("user_password")
+	private String password;
+
+	@NotEmpty(message = "닉네임은 필수 입력 항목입니다.")
+	@JsonProperty("user_nickname")
+	private String nickname;
+
+	@JsonProperty("user_profileImage")
+	private MultipartFile profileImage;
+}
---
+++ src/main/java/org/example/gangazido_be/user/entity/User.java
@@ -0,0 +1,46 @@
+package org.example.gangazido_be.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Integer id;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false, unique = true)
+	private String nickname;
+
+	@Column(name = "profile_image")
+	private String profileImage;
+
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	private LocalDateTime deletedAt;
+
+	@PrePersist
+	protected void onCreate() {
+		createdAt = LocalDateTime.now();
+		updatedAt = LocalDateTime.now();
+	}
+
+	@PreUpdate
+	protected void onUpdate() {
+		updatedAt = LocalDateTime.now();
+	}
+}
