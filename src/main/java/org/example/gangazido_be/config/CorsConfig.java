a3d55e3a2936ef4d2b0c48e2438a31ca09dd20d4
Gdugboy <gdugboy@gmail.com>
2025-03-25 13:36:37 +0900

    fix: Checkstyle fix

--- src/main/java/org/example/gangazido_be/config/CorsConfig.java
+++ src/main/java/org/example/gangazido_be/config/CorsConfig.java
@@ -8,17 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 모든 경로에 대해
-                        .allowedOrigins("http://localhost:3000", "https://www.gangazido.com") // 허용할 프론트 주소
-                        .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
-                        .allowCredentials(true) // 세션/쿠키 포함 시 true
-                        .allowedHeaders("*");
-            }
-        };
-    }
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // 모든 경로에 대해
+					.allowedOrigins("http://localhost:3000", "https://www.gangazido.com") // 허용할 프론트 주소
+					.allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+					.allowCredentials(true) // 세션/쿠키 포함 시 true
+					.allowedHeaders("*");
+			}
+		};
+	}
 }
