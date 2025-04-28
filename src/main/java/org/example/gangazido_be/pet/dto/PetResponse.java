677845d7def1171d6218a7e0508fef07b9a85ec7
Myungbeem Kim <128389329+mbeankong@users.noreply.github.com>
2025-04-28 10:02:44 +0900

    fix: 반려견 필수 필드 누락 시 404 에러 반환하도록 수정

--- src/main/java/org/example/gangazido_be/pet/dto/PetResponse.java
+++ src/main/java/org/example/gangazido_be/pet/dto/PetResponse.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import org.example.gangazido_be.pet.entity.Pet;
+import org.example.gangazido_be.pet.exception.PetException;
+import org.example.gangazido_be.pet.exception.PetExceptionType;
+import org.springframework.http.HttpStatus;
 
 @Setter
 @Getter
@@ -18,6 +21,10 @@ public class PetResponse {
 	private Double weight;
 
 	public static PetResponse from(Pet pet) {
+		if (pet.getName() == null || pet.getAge() == null || pet.getGender() == null || pet.getBreed() == null || pet.getWeight() == null) {
+			throw new PetException(HttpStatus.NOT_FOUND, PetExceptionType.NOT_FOUND_PET.getMessage());
+		}
+
 		return PetResponse.builder()
 			.name(pet.getName())
 			.profileImage(pet.getProfileImage())
